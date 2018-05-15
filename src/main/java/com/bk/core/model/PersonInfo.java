03f17e439d47709774b4dd687515f529395cab58
Markz <415752133@QQ.com>
2018-05-15 11:12:26 +0800

    test for new branch

--- src/main/java/com/bk/core/model/PersonInfo.java
+++ src/main/java/com/bk/core/model/PersonInfo.java
@@ -6,4 +6,19 @@ public class PersonInfo {
 
     private int age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
