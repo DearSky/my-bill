49fb3a16d13df14aaa4b8e047411b29d5b291281
Markz <415752133@QQ.com>
2018-05-15 11:28:36 +0800

    新建一个分支

---
+++ src/main/java/com/bk/core/model/HomeInfo.java
@@ -0,0 +1,24 @@
+package com.bk.core.model;
+
+public class HomeInfo {
+
+    private String location;
+
+    private String locationCode;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLocationCode() {
+        return locationCode;
+    }
+
+    public void setLocationCode(String locationCode) {
+        this.locationCode = locationCode;
+    }
+}
